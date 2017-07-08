6071e78f559b64b6eaabb98cb818429b0c44ea25
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-07-08 19:20:16 -0400

    .gitignore

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import redis
 import sys
 import json
 
@@ -7,23 +8,28 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+db = redis.from_url(os.environ.get("REDIS_URL"))
+
+
+#frontend
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
+#verify
 @app.route('/webhook', methods=['GET'])
 def verify():
-	if request.args["hub.mode"] == "subscribe":
+	if request.args["hub.mode"] == "subscribe": #will later need to learn how to hide important things like this
 		if not request.args["hub.verify_token"] == 'verifyMe':
 			return "Verification token mismatch", 403
 		return request.args["hub.challenge"], 200
 
-'''
+
+#message logic
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	if method == 'GET':
-'''
+	
+
 
 
 if __name__ == '__main__':
