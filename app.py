99dcba58bd7461ef93c08b7e5f003c559d84f3dc
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-07-08 18:51:03 -0400

    Webhook verification

--- app.py
+++ app.py
@@ -12,5 +12,19 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/webhook', methods=['GET'])
+def verify():
+	if request.args["hub.mode"] == "subscribe":
+		if not request.args["hub.verify_token"] == 'verifyMe':
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+
+'''
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	if method == 'GET':
+'''
+
+
 if __name__ == '__main__':
 	app.run()
