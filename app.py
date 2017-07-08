ebc0c7505ca1a2940b1117fb7465faa1ec46b76c
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-07-08 19:57:25 -0400

    Queue'd up

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 
 import requests
 from flask import Flask, request
+#APScheduler
 
 app = Flask(__name__)
 
@@ -21,15 +22,34 @@ def hello_world():
 def verify():
 	if request.args["hub.mode"] == "subscribe": #will later need to learn how to hide important things like this
 		if not request.args["hub.verify_token"] == 'verifyMe':
-			return "Verification token mismatch", 403
+			return "Verification token incorrect", 403
 		return request.args["hub.challenge"], 200
 
 
 #message logic
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	
 
+	messageObject = request.get_json()
+	log(messageObject)
+
+	#message parse logic
+
+
+
+#periodic message send, uses database APScheduler
+
+
+
+#at a time of day cache the menus, APScheduler
+
+
+
+
+#logs messages
+def log(message):
+	print str(message)
+	sys.stdout.flush()
 
 
 if __name__ == '__main__':
