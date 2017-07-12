45a526acb9d8c80ad2e19674c3d606673b83c535
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-07-12 19:13:42 -0400

    Fixed bugs

--- app.py
+++ app.py
@@ -9,27 +9,89 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-db = redis.from_url(os.environ.get("REDIS_URL"))
 
-'''
+#url = os.environ.get("REDIS_URL")
+#print(url)
+db = redis.from_url('redis://h:p3116b29cf75492a50fe130ffeb19d111fe87d4b0daea9440e235fec5a5f14300@ec2-34-224-49-43.compute-1.amazonaws.com:45779')
+db.set_response_callback('GET',int)
+
+
+
 headers = {
 	"Content-Type": "application/json"
 }
-'''
+
+#for testing###################################################
+db.flushdb()
+
+deleteMessagePayload = {
+  "fields":[
+    "greeting"
+  ]
+}
+try:
+	deleteMessageResponse = requests.delete("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data=json.dumps(deleteMessagePayload), headers=headers)
+	if (deleteMessageResponse.status_code != requests.codes.ok):
+		raise ConnectionError("Greeting delete failed!")
+	else:
+		print("Greeting deleted successfully!")
+except ConnectionError as err:
+	print(err) 
+
+getGreeting = requests.get("https://graph.facebook.com/v2.6/me/messenger_profile?fields=greeting&access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6")
+print (getGreeting.json(), end="\n\n")
+
+
+deletegetStartedPayload = {
+  "fields":[
+    "get_started"
+  ]
+}
+try:
+	deletegetStartedResponse = requests.delete("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data=json.dumps(deletegetStartedPayload), headers=headers)
+	if (deletegetStartedResponse.status_code != requests.codes.ok):
+		raise ConnectionError("Get started delete failed!")
+	else:
+		print("Get started deleted successfully!")
+except ConnectionError as err:
+	print(err) 
+
+getStarted = requests.get("https://graph.facebook.com/v2.6/me/messenger_profile?fields=get_started&access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6")
+print (getStarted.json(), end="\n\n\n")
+
+
+
+#######################################################################
+
+
+
 
 #initialize get started button and greeting text
 greetingsPayload = {
-  "setting_type":"greeting",
-  "greeting":{
-    "text":"University of Michigan Dining hall menus straight to your phone, every morning."
-  }
-}
+  "greeting":[
+    {
+      "locale":"default",
+      "text":"Hello!"
+    }
+  ] 
+} 
+print(json.dumps(greetingsPayload))
 try:
-	greetingResponse = requests.post("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data = json.dumps(greetingsPayload))
-	if (greetingResponse.json() != requests.codes.ok):
-		raise ConnectionError("greeting set failed")
+	greetingResponse = requests.post("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data=json.dumps(greetingsPayload), headers=headers)
+	#print (greetingResponse.status_code)
+	print(greetingResponse.json())
+	if (greetingResponse.status_code != requests.codes.ok):
+		raise ConnectionError("Greeting set failed!")
+	else:
+		print("Greeting set successfully!")
 except ConnectionError as err:
-	print(err.args) 
+	print(err) 
+
+
+
+getGreeting = requests.get("https://graph.facebook.com/v2.6/me/messenger_profile?fields=greeting&access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6")
+print (getGreeting.json(), end="\n\n")
+
 
 
 getStartedPayload = { 
@@ -37,12 +99,21 @@ getStartedPayload = {
     "payload":"GET_STARTED_PAYLOAD"
   }
 }
+print(json.dumps(getStartedPayload))
 try:
-	getStartedResponse = requests.post("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data = json.dumps(getStartedPayload)) 
+	getStartedResponse = requests.post("https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6", data=json.dumps(getStartedPayload), headers=headers) 
+	#print (getStartedResponse.status_code)
+	print(getStartedResponse.json())
 	if (getStartedResponse.status_code != requests.codes.ok):
 		raise ConnectionError("get started set failed")
+	else:
+		print("GetStarted set successfully!")
 except ConnectionError as err:
-	print(err.args) 
+	print(err) 
+
+
+getStarted = requests.get("https://graph.facebook.com/v2.6/me/messenger_profile?fields=get_started&access_token=EAAB6sqmI7uwBAOk4EZBGAgB67ZA40ziA7T5r82TUiXZAFnacYHcuK5KRFsVHy7le7SrUzmCfJQVamZBArsAzTcdGSeUzrUNPTh8V3PeIuVfKto2f43eqK4aj2Mk72J95e1HKj9SHVerm3ZAG20dT9SPAGO8b3u6ZCoZBv8tcuQHmhvyYH0EqiH6")
+print (getStarted.json())
 
 
 
@@ -73,9 +144,11 @@ def verify():
 def webhook():
 
 	messageObject = request.get_json()
-	log(messageObject)
+	print(messageObject)
 
 	#message parse logic
+	if not db.exists(messageObject["entry"]["messaging"]["sender"]["id"]):
+		db.set(messageObject["entry"]["messaging"]["sender"]["id"], 0)
 
 
 
@@ -90,7 +163,7 @@ def webhook():
 
 #logs messages
 def log(message):
-	print str(message)
+	print (str(message))
 	sys.stdout.flush()
 
 
