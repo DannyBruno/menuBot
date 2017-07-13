f1c93352acdd69f22bdb1e3b71f79578180ce4bd
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-07-13 19:31:16 -0400

    hi

--- app.py
+++ app.py
@@ -161,6 +161,7 @@ def webhook():
 
 	print(messageObject)
 	if 'postback' in body:	#get started was triggered
+		print("HELLO!")
 		sendMessage(senderID,"Hi {{user first name}}! Welcome to menuBot! Would you like to subscribe to the service? (Yes,No)")
 		db.set(senderID, 0)
 	else:
