089be0d7279e1ace7f2b646612d1f6cae8d729aa
Dan LaBruna <DanielSLaBruna@gmail.com>
2017-08-27 10:41:08 -0700

    Stop pinging

--- clock.py
+++ clock.py
@@ -22,19 +22,21 @@ secret = {
 
 @scheduler.scheduled_job('cron', hour=3, minute=21, second=10, timezone=pytz.timezone('US/Eastern'))
 def startCaching():
+	print("Hmm")
 	#request
 	payload = {
 		'request': 'cache'
 	}
-	requests.post(request.url_root + "/request", params=secret, data=json.dumps(payload), headers=headers)
+	#requests.post(request.url_root + "/request", params=secret, data=json.dumps(payload), headers=headers)
 
 @scheduler.scheduled_job('cron', hour=3, minute=21, second=45, timezone=pytz.timezone('US/Eastern'))
 def startSending():
+	print("What")
 	#request
 	payload = {
 		'request': 'send'
 	}
-	requests.post(request.url_root + "/request", params=secret, data=json.dumps(payload), headers=headers)
+	#requests.post(request.url_root + "/request", params=secret, data=json.dumps(payload), headers=headers)
 
 
 scheduler.start()
